b42d4ccd5534099b5eb609f147cc8845611531f9
Tasleem <tasleembca31@gmail.com>
2022-07-13 18:23:50 +0530

    added angular, react

---
+++ poc-angular/src/app/a-com/a-com.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AComComponent } from './a-com.component';
+
+@NgModule({
+  declarations: [AComComponent],
+  imports: [CommonModule],
+})
+export class AComModule {}
